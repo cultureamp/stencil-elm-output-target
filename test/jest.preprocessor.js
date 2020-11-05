6c5a775ed60d875f606403bdd162ee6026812881
Kevin Yank <thatguy@kevinyank.com>
2020-11-05 14:02:00 +1100

    Fix error running Jest after npm build
    The production build was including Jest tests in the compiled output,
    which was causing Jest to run those compiled tests along with the
    originals in the source directory, and failing due to the ES imports
    in the compiled versions.
    
    This creates a special TypeScript config file for npm run build that
    excludes the tests, and enables the "main" TypeScript config to produce
    the Jest-compatible CommonJS modules.

--- test/jest.preprocessor.js
+++ test/jest.preprocessor.js
@@ -1,9 +1,6 @@
 const tsc = require('typescript');
 const tsConfig = require('../tsconfig.json');
 
-// force the output to use commonjs modules required by jest
-tsConfig.compilerOptions.module = 'commonjs';
-
 module.exports = {
   process(src, path) {
     if (path.endsWith('.ts') || path.endsWith('.tsx')) {
